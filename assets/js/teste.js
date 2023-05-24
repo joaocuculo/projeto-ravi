9b86c737ea9078f4ceb1f5ee51bb0f03a8a6a28b
João Victor Codato Cuculo <123258227+joaocuculo@users.noreply.github.com>
2023-05-24 16:27:51 -0300

    ajustando cabeçalho

---
+++ assets/js/teste.js
@@ -0,0 +1,11 @@
+function menuShow() {
+    let menuResponsivo = document.querySelector('.menu-responsivo');
+    
+    if (menuResponsivo.classList.contains('open')) {
+        menuResponsivo.classList.remove('open');
+        document.querySelector('.icon').src = "assets/img/menu_white_36dp.svg";
+    } else {
+        menuResponsivo.classList.add('open');
+        document.querySelector('.icon').src = "assets/img/close_white_36dp.svg"
+    }
+}
