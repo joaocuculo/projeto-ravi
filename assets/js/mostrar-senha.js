246f21391e0321cfa86499d7f821e4d9eb7563d4
João Victor Codato Cuculo <123258227+joaocuculo@users.noreply.github.com>
2023-05-28 20:39:08 -0300

    arrumando senha-conf

--- assets/js/mostrar-senha.js
+++ assets/js/mostrar-senha.js
@@ -10,3 +10,16 @@ function mostrarSenha() {
         btnMostrarSenha.classList.replace('fa-eye-slash', 'fa-eye')
     }
 }
+
+function mostrarSenhaConf() {
+    var inputSenhaConf = document.getElementById('senha-conf');
+    var btnMostrarSenhaConf = document.getElementById('btn-senha-conf');
+
+    if (inputSenhaConf.type === 'password') {
+        inputSenhaConf.setAttribute('type', 'text');
+        btnMostrarSenhaConf.classList.replace('fa-eye', 'fa-eye-slash')
+    } else {
+        inputSenhaConf.setAttribute('type', 'password');
+        btnMostrarSenhaConf.classList.replace('fa-eye-slash', 'fa-eye')
+    }
+}
