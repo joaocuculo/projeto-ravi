136fbf92d98ac565a34e600f33c8b587617a8eb2
João Victor Codato Cuculo <123258227+joaocuculo@users.noreply.github.com>
2023-07-09 16:27:23 -0300

    cidade e estado criados

--- cadastro-aluno.php
+++ cadastro-aluno.php
@@ -113,6 +113,44 @@
                             <label for="CEP">CEP</label>
                             <input type="text" name="CEP" id="CEP" placeholder="Digite seu CEP" minlength="9" maxlength="9" required>
                         </div>
+                        <div class="input-box input-meio">
+                            <label for="estado">Estado</label>
+                            <select name="estado" id="estado" onchange="buscaCidades(this.value)">
+                                <option value="">Selecione o Estado</option>
+                                <option value="AC">Acre</option>
+                                <option value="AL">Alagoas</option>
+                                <option value="AP">Amapá</option>
+                                <option value="AM">Amazonas</option>
+                                <option value="BA">Bahia</option>
+                                <option value="CE">Ceará</option>
+                                <option value="DF">Distrito Federal</option>
+                                <option value="ES">Espírito Santo</option>
+                                <option value="GO">Goiás</option>
+                                <option value="MA">Maranhão</option>
+                                <option value="MT">Mato Grosso</option>
+                                <option value="MS">Mato Grosso do Sul</option>
+                                <option value="MG">Minas Gerais</option>
+                                <option value="PA">Pará</option>
+                                <option value="PB">Paraíba</option>
+                                <option value="PR">Paraná</option>
+                                <option value="PE">Pernambuco</option>
+                                <option value="PI">Piauí</option>
+                                <option value="RJ">Rio de Janeiro</option>
+                                <option value="RN">Rio Grande do Norte</option>
+                                <option value="RS">Rio Grande do Sul</option>
+                                <option value="RO">Rondônia</option>
+                                <option value="RR">Roraima</option>
+                                <option value="SC">Santa Catarina</option>
+                                <option value="SP">São Paulo</option>
+                                <option value="SE">Sergipe</option>
+                                <option value="TO">Tocantins</option>
+                            </select>
+                        </div>
+                        <div class="input-box input-meio">
+                            <label for="cidade">Cidade</label>
+                            <select name="cidade" id="cidade">
+                            </select>
+                        </div>
                     </div>
                     <div class="input-box">
                         <label for="endereco">Endereço</label>
