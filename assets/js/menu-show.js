fba2e5fd5980bdc401eebfc71c0918bf59f19ab2
João Victor Codato Cuculo <123258227+joaocuculo@users.noreply.github.com>
2023-08-06 15:03:34 -0300

    css sobre nos feito

--- assets/js/menu-show.js
+++ assets/js/menu-show.js
@@ -3,9 +3,9 @@ function menuShow() {
     
     if (menuResponsivo.classList.contains('open')) {
         menuResponsivo.classList.remove('open');
-        document.querySelector('.icon').src = "assets/img/menu_white_36dp.svg";
+        document.querySelector('.icon').src = "../assets/img/menu_white_36dp.svg";
     } else {
         menuResponsivo.classList.add('open');
-        document.querySelector('.icon').src = "assets/img/close_white_36dp.svg"
+        document.querySelector('.icon').src = "../assets/img/close_white_36dp.svg"
     }
 }
