8f02907cbca3f4ea44bc3f8fab9234fe967a18d3
João Victor Codato Cuculo <123258227+joaocuculo@users.noreply.github.com>
2023-11-07 23:28:36 -0300

    tentando entender calendario

--- assets/js/calendario_estatic_events.js
+++ assets/js/calendario_estatic_events.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', function() {
       
       //Definir o idioma usado no calendario
       locale: 'pt-br',
-      //initialDate: '2023-01-12',
+      initialDate: '2023-01-12',
       //Permitir clicar nos nomes dos dias da semana
       navLinks: true,
       //Permitir clicar e arrastar o mouse sobre um ou varios dias no calendario
@@ -46,7 +46,62 @@ document.addEventListener('DOMContentLoaded', function() {
 
       //Número máximo de eventos em um determinado dia, se for true, o número de eventos será limitado à altura da célula do dia
       dayMaxEvents: true, // allow "more" link when too many events
-      events: 'listar-evento.php'
+      events: [
+        {
+          title: 'All Day Event',
+          start: '2023-01-01'
+        },
+        {
+          title: 'Long Event',
+          start: '2023-01-07',
+          end: '2023-01-10'
+        },
+        {
+          groupId: 999,
+          title: 'Repeating Event',
+          start: '2023-01-09T16:00:00'
+        },
+        {
+          groupId: 999,
+          title: 'Repeating Event',
+          start: '2023-01-16T16:00:00'
+        },
+        {
+          title: 'Conference',
+          start: '2023-01-11',
+          end: '2023-01-13'
+        },
+        {
+          title: 'Meeting',
+          start: '2023-01-12T10:30:00',
+          end: '2023-01-12T12:30:00'
+        },
+        {
+          title: 'Lunch',
+          start: '2023-01-12T12:00:00'
+        },
+        {
+          title: 'Meeting',
+          start: '2023-01-12T14:30:00'
+        },
+        {
+          title: 'Happy Hour',
+          start: '2023-01-12T17:30:00'
+        },
+        {
+          title: 'Dinner',
+          start: '2023-01-12T20:00:00'
+        },
+        {
+          title: 'Birthday Party',
+          start: '2023-01-13T07:00:00'
+        },
+        {
+          title: 'Click for Google',
+          url: 'http://google.com/',
+          start: '2023-01-28'
+        }
+      ]
     });
 
     calendar.render();
---
+++ sistema-aluno/aluno/listar-evento.php
@@ -0,0 +1,31 @@
+<?php
+
+    include_once './conexao-agenda.php';
+
+    //query para recuperar os eventos
+    $query_events = "SELECT id, title, color, start, end FROM events";
+
+    //prepara a query
+    $result_events = $conn->prepare($query_events);
+
+    //executando a query
+    $result_events->execute();
+
+    //criar array que recebe os eventos
+    $eventos = [];
+
+    //percorre a lista de registros retornado do banco de dados
+    while($row_events = $result_events->fetch(PDO::FETCH_ASSOC)) {
+        //extrair o array
+        extract($row_events);
+        
+        $eventos[] = [
+            'id' => $id,
+            'title' => $title,
+            'color' => $color,
+            'start' => $start,
+            'end' => $end
+        ];
+    }
+
+    echo json_encode($eventos);
