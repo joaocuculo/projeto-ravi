55d0fcac27364d47484170163124f983bf13361a
João Victor Codato Cuculo <123258227+joaocuculo@users.noreply.github.com>
2023-11-29 00:20:36 -0300

    tela principal do prof

--- sistema-professor/professor/principal.php
+++ sistema-professor/professor/principal.php
@@ -2,6 +2,14 @@
 
     require_once('../verifica-autenticacao.php');
     
+    require_once('../../conexao.php');
+    
+    $sql = "SELECT events.*, aluno.nome
+              FROM events
+        INNER JOIN aluno ON events.aluno_id = aluno.id
+             WHERE professor_id = " . $_SESSION['id'];
+    $resultado = mysqli_query($conexao, $sql);
+
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -20,7 +28,7 @@
 
     <main class="container">
         <div class="bem-vindo">
-            <h2>Seja bem vindo, <?= $_SESSION['nome']; ?></h2>
+            <h2>Seja bem vindo, <?= $_SESSION['nome']; ?>. Confira as matérias</h2>
         </div>
 
         <section class="cursos">
@@ -53,6 +61,27 @@
                         <i class="fa-sharp fa-solid fa-circle-arrow-right" style="color: #000000;"></i>
                     </a>    
                 </article>
+            </div>
+        </section>
+
+        <section style="margin-top: -6rem;">
+            <div class="bem-vindo">
+                <h2>Aulas Agendadas</h2>
+            </div>
+
+            <div class="card-agend">
+                <?php while ($linha = mysqli_fetch_array($resultado)) { ?>
+                    <?php
+                        $inicio = new DateTime($linha['start']);
+                        $fim = new DateTime($linha['end']);
+                    ?>
+                    <article style="background-image: linear-gradient(to left, #D9D7CA 0, #D9D7CA 95%, <?= $linha['color'] ?> 5%);">
+                        <h3><?= $linha['title'] ?></h3>
+                        <h5>Aluno(a) <?= $linha['nome'] ?></h5>
+                        <h5>Início: <?= $inicio->format('d/m/Y H:i:s') ?></h5>
+                        <h5>Fim: <?= $fim->format('d/m/Y H:i:s') ?></h5>
+                    </article>
+                <?php } ?>    
             </div>
         </section>
     </main>
